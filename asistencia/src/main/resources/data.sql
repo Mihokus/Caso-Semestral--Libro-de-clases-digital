0d5c1f4efc784ace54feab4859a93b99c78add7c
Cristopher Barrueto <cr.barrueto@duocuc.cl>
2026-04-22 01:00:26 -0400

    Final: Agregado AlumnoController y corrección de seguridad

---
+++ asistencia/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO alumnos (rut, nombre, curso) VALUES ('11111111-1', 'Cristopher Barrueto', 'Informatica');
+INSERT INTO alumnos (rut, nombre, curso) VALUES ('22222222-2', 'Juan Perez', 'Matematicas');
