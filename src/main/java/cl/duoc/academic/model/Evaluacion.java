ae78a47679bf341b7be45690c94e406accf5d314
Mihokus <seba.rodriguezg@duocuc.cl>
2026-04-23 20:26:59 -0400

    feat: creacion de entidades para asignaturas y evaluaciones

---
+++ src/main/java/cl/duoc/academic/model/Evaluacion.java
@@ -0,0 +1,19 @@
+package cl.duoc.academic.model;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Evaluacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombreEvaluacion;
+    private Double nota;
+    private Double ponderacion;
+    @ManyToOne
+    @JoinColumn(name = "asignatura_id")
+    private Asignatura asignatura;
+
+    
+}
