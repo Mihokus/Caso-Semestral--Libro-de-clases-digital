88ab4dc8469d37d2b0985433a0abc0823d74ac24
Mihokus <seba.rodriguezg@duocuc.cl>
2026-04-24 23:13:58 -0400

    feat: Implementacion de rest para la api de gestion academica

---
+++ src/main/java/cl/duoc/academic/controller/AcademicController.java
@@ -0,0 +1,31 @@
+package cl.duoc.academic.controller;
+import cl.duoc.academic.dto.EvaluacionDTO;
+import cl.duoc.academic.facade.AcademicFacade;
+import cl.duoc.academic.model.Evaluacion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/academica")
+public class AcademicController{
+
+    @Autowired
+    private AcademicFacade academicFacade;
+
+    @PostMapping("/registrarNotas");
+    public ResponseEntity<Evaluacion> registrarNota(@RequestBody EvaluacionDTO dto){
+        Evaluacion nuevaNota = academicFacade.registrarNota(
+            dto.getAsignaturaId();
+            dto.getNombre();
+            dto.getNota();
+        );
+        return ResponseEntity.ok(nuevaNota);
+    }
+    
+    @GetMapping("/rendimiento/{asignaturaId}")
+    public ResponseEntity<Double> obtenerRendimiento(@PathVariable Long asignaturaId) {
+        Double promedio = academicFacade.obtenerPromedioRendimiento(asignaturaId);
+        return ResponseEntity.ok(promedio);
+    }
+}
