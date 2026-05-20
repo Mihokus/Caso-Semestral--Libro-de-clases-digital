1eb871d440cab45513f2edc86916b1f306bc4bed
XxCristopherxXC <cr.barrueto@duocuc.cl>
2026-05-20 19:11:36 -0400

    Delete academic-service directory

--- academic-service/src/main/java/cl/duoc/academic/repository/AsignaturaRepository.java
+++
@@ -1,9 +0,0 @@
-package cl.duoc.academic.repository;
-import cl.duoc.academic.model.Asignatura;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AsignaturaRepository extends JpaRepository<Asignatura, Long>{
-
-}
