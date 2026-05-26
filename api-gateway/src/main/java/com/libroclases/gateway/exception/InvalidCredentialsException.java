836197c5442f5bab2c9fd4790719d901babcc887
Mihokus <seba.rodriguezg@duocuc.cl>
2026-05-25 21:15:11 -0400

    Delete api-gateway directory

--- api-gateway/src/main/java/com/libroclases/gateway/exception/InvalidCredentialsException.java
+++
@@ -1,8 +0,0 @@
-package com.libroclases.gateway.exception;
-
-public class InvalidCredentialsException extends RuntimeException {
-
-    public InvalidCredentialsException(String message) {
-        super(message);
-    }
-}
