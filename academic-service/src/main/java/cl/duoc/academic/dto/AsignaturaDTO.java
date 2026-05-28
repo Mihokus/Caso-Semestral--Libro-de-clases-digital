0a5c95a546d54cb642d76521eaa200c29cb0f122
XxCristopherxXC <cr.barrueto@duocuc.cl>
2026-05-27 20:38:42 -0400

    Delete academic-service directory

--- academic-service/src/main/java/cl/duoc/academic/dto/AsignaturaDTO.java
+++
@@ -1,9 +0,0 @@
-package cl.duoc.academic.dto;
-
-import lombok.Data;
-
-@Data
-public class AsignaturaDTO {
-    private String nombre;
-    private String curso;
-}
--- academic-service/src/main/java/cl/duoc/academic/repository/EvaluacionRepository.java
+++
@@ -1,9 +0,0 @@
-package cl.duoc.academic.repository;
-import cl.duoc.academic.model.Evaluacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import java.util.List;
-
-public interface EvaluacionRepository  extends JpaRepository<Evaluacion, Long>{
-    List<Evaluacion> findByAsignaturaId (Long asignaturaId);
-}
